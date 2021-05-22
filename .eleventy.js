ece49a55c6035aec35c855cabd2ac15621e079fc
kuspoes <kusaeni@gmail.com>
2021-05-22 08:28:30 +0700

    benerin tipis2

--- .eleventy.js
+++ .eleventy.js
@@ -39,43 +39,36 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLiquidShortcode("related", async function (judul) {
 		try {
             const response = await fetch('https://kusaeni.com/baca/data.json');
-			const data     = await response.json();
+            const data     = await response.json();
             const hasilData= data.find(function(caridata) {
                 return caridata.title.toLowerCase() === judul.toLowerCase()
             }); 
             //console.log(hasilData)
             var rese = await hasilData.resensi.substr(0, 200)
-			return `<div class="flex flex-row border border-gray-400 rounded-xl w-99 mx-auto mb-6 p-6 font-sans">
-				<img class="shadow-md" src="${hasilData.coverImg}" width="110" height="130" >
-				<div class="flex-1 w-1/2 pl-8 text-lg text-gray-700"> 
-					<b><a href="${hasilData.url}" title="${hasilData.title}">${hasilData.title}</a> </b>
-					<dl>
-						<dt>${hasilData.penulis} </dt> 
-						<dd>${rese} ...</dd>
-					</dl>
-				</div>
-				</div>`;
-		} catch (err) {
-			console.log(err)
-		}
-		const print = async () => {
-			const p = await hasilData;
-			//console.log(p)
-		};
-		print()
-	});
+            return `<div class="flex flex-row border border-gray-400 rounded-xl w-99 mx-auto mb-6 p-6 font-sans">
+                <img class="shadow-md" src="${hasilData.coverImg}" width="110" height="130" >
+                <div class="flex-1 w-1/2 pl-8 text-lg text-gray-700"> 
+                <b><a href="${hasilData.url}" title="${hasilData.title}">${hasilData.title}</a> </b>
+                <dl><dt>${hasilData.penulis}</dt><dd>${rese} ...</dd>
+                </dl></div></div>`;
+            } catch (err) {
+                console.log(err)
+        }
+        const print = async () => {
+         const p = await hasilData;
+        };
+        print()
+    });
 
 
     eleventyConfig.addPairedShortcode("relatedpair", function(resensi, coverImg, judul, url){
         let coverUrl = "https://ik.imagekit.io/hjse9uhdjqd/tr:n-cover/buku/"
         return `<div class="flex flex-row border border-gray-400 rounded-xl w-99 mx-auto mb-6 p-6 font-sans">
-				<img class="shadow-md" src="${coverUrl}${coverImg}" width="110" height="130" >
-				<div class="flex-1 w-1/2 pl-8 text-lg text-gray-700"> 
-					<b><a href="${url}" title="${judul}">${judul}</a> </b>
-					<dd>${resensi} ...</dd>
-					</dl>
-				</div>
-				</div>`;
+        <img class="shadow-md" src="${coverUrl}${coverImg}" width="110" height="130" >
+        <div class="flex-1 w-1/2 pl-8 text-lg text-gray-700"> 
+        <b><a href="${url}" title="${judul}">${judul}</a></b>
+        <dd>${resensi} ...</dd>
+        </dl></div></div>`;
     });
 
     // post-related article
@@ -87,37 +80,34 @@ module.exports = function(eleventyConfig) {
         </div>`;
     });
 
-	// markdownIt
-	let markdownLibrary = markdownIt({
-		html: true,
-		breaks: false,
-		linkify: true,
+    // markdownIt
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: false,
+        linkify: true,
         typographer: true
-	});
+    });
     eleventyConfig.setLibrary("md", markdownLibrary.use(mdatrs, {
         allowedAttrubutes: ['id', 'class']
     }));
-	eleventyConfig.addFilter('mdParse', function(resensi) {
-		let mdp = new markdownIt({
-			html: true,
-            typographer: true
-		});
-		return mdp.render(resensi)
-	})
+    eleventyConfig.addFilter('mdParse', function(resensi) {
+        let mdp = new markdownIt({html: true, typographer: true });
+        return mdp.render(resensi)
+    })
 
-		// syntax highlighting
-	eleventyConfig.addPlugin(syntaxHighlight);
+    // syntax highlighting
+    eleventyConfig.addPlugin(syntaxHighlight);
 
-	// syntax highlighting
-	//eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    // syntax highlighting
+    //eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-	// rss
-	eleventyConfig.addPlugin(pluginRss);
+    // rss
+    eleventyConfig.addPlugin(pluginRss);
 
     // reader bar
     eleventyConfig.addPlugin(readerBar)
 
-	// embed twitter
+    // embed twitter
     eleventyConfig.addPlugin(embedTwitter);
 
     //eleventyConfig.addPlugin(generateSocialImages, {
@@ -130,32 +120,29 @@ module.exports = function(eleventyConfig) {
     //});
 
     // Next Prev 
-	eleventyConfig.addCollection("baca", function (collection) {
-		const coll = collection.getFilteredByTag("baca");
+    eleventyConfig.addCollection("baca", function (collection) {
+        const coll = collection.getFilteredByTag("baca");
+        for (let i = 0; i < coll.length; i++) {
+            const bacaSebelumnya = coll[i - 1];
+            const bacaSelanjutnya = coll[i + 1];
 
-		for (let i = 0; i < coll.length; i++) {
-			const bacaSebelumnya = coll[i - 1];
-			const bacaSelanjutnya = coll[i + 1];
+            coll[i].data["bacaSebelumnya"] = bacaSebelumnya;
+            coll[i].data["bacaSelanjutnya"] = bacaSelanjutnya;}
+        return coll;
+    });
 
-			coll[i].data["bacaSebelumnya"] = bacaSebelumnya;
-			coll[i].data["bacaSelanjutnya"] = bacaSelanjutnya;
-		}
-		return coll;
-	});
+    // Next Prev 
+    eleventyConfig.addCollection("jurnal", function (collection) {
+        const post = collection.getFilteredByTag("jurnal");
+        for (let i = 0; i < post.length; i++) {
+            const prevPost = post[i - 1];
+            const nextPost = post[i + 1];
 
-	// Next Prev 
-	eleventyConfig.addCollection("jurnal", function (collection) {
-		const post = collection.getFilteredByTag("jurnal");
-
-		for (let i = 0; i < post.length; i++) {
-			const prevPost = post[i - 1];
-			const nextPost = post[i + 1];
-
-			post[i].data["prevPost"] = prevPost;
-			post[i].data["nextPost"] = nextPost;
-		}
-		return post;
-	});
+            post[i].data["prevPost"] = prevPost;
+            post[i].data["nextPost"] = nextPost;
+        }
+        return post;
+    });
 
     eleventyConfig.addCollection("postByYear", (collection) => {
         return _.chain(collection.getFilteredByTag("jurnal"))
@@ -167,19 +154,13 @@ module.exports = function(eleventyConfig) {
 
     // Biar mudah dicomment
     // lazy images
-	//eleventyConfig.addPlugin(lazyImagesPlugin);
-/*
+    //eleventyConfig.addPlugin(lazyImagesPlugin);
+
     // compress html output
     eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-		if (outputPath.endsWith(".html")) {
-			let minified = htmlmin.minify(content, {
-				useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true
-			});
-			return minified;
-		}
-		return content;
+    if (outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, { useShortDoctype: true, removeComments: true, collapseWhitespace: true });
+        return minified;}
+        return content;
     });
-    */
 };
